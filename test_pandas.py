50e924009ed79a931dbc4e004500e667f2eedeb4
masahiro.shimmori <shinmasa0412@yahoo.co.jp>
2022-03-05 13:21:32 +0900

    test

--- test_pandas.py
+++ test_pandas.py
@@ -1,8 +1,10 @@
+from asyncio.windows_events import NULL
 import pandas as pd
 
 def main():
 
     l_id = pd.read_csv('csv/id.csv')
+    l_id.fillna({'city':0}, inplace=True)
     print(l_id)
 
 if __name__ == "__main__":
