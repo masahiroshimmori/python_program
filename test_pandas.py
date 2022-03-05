4d1693c3f9365fd2d47b5f2c64947ec5dbda2751
masahiroshimmori <shinmasa0412@yahoo.co.jp>
2022-03-05 13:20:38 +0900

    test

--- test_pandas.py
+++ test_pandas.py
@@ -3,7 +3,10 @@ import pandas as pd
 def main():
 
     l_id = pd.read_csv('csv/id.csv')
-    print(l_id)
+
+    none_pd = l_id.where(l_id.notnull(), None)
+
+    print(none_pd)
 
 if __name__ == "__main__":
     main()
