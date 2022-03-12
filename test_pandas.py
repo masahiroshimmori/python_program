6843859a1f4255af31c2b797641b1831aa54b27c
masahiro.shimmori <shinmasa0412@yahoo.co.jp>
2022-03-12 14:37:01 +0900

    comit

--- test_pandas.py
+++ test_pandas.py
@@ -8,6 +8,10 @@ def main():
     none_pd = l_id.where(l_id.notnull(), None)
 
     print(none_pd)
-
+    #none_pd_records = none_pd.to_dict('records')
+   #for none_pd_record in none_pd_records:
+    #    if isinstance(none_pd_record["pre"], (int,float)):
+     #       none_pd_record["order_no"] = 0
+     #       print(none_pd_record["order_no"])
 if __name__ == "__main__":
     main()
