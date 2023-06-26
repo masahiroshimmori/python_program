269c0fa8e49e258626125621e85057281f326f57
masahiro.shimmori <shinmasa0412@yahoo.co.jp>
2023-06-26 22:47:49 +0900

    comittte

--- static_test.py
+++ static_test.py
@@ -3,7 +3,7 @@ from testclass import static_class
 def main():
     # インスタンスを2つ生成
     my1 = static_class.Error_info()
-    rtn = my1.rtn_counter() # 1
+    rtn = my1.rtn_counter() # 2
     print(rtn)
     my2 = static_class.Error_info()
     rtn = my2.rtn_counter() # 2
